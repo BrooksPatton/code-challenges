1404aa69076e179df35216cc67a9729ed6ff48f9
Brooks Patton <brooks@brookzerker.com>
2018-12-02 21:38:18 -0700

    got the second star of day two!

--- advent_of_code/2018/day_two/src/main.rs
+++ advent_of_code/2018/day_two/src/main.rs
@@ -1,66 +1,88 @@
 use std::fs::File;
-use std::io::prelude::*;
-use std::collections::HashMap;
+use std::io::Read;
 
-struct Counts {
-    two: u32,
-    three: u32
+struct PartTwo {
+    puzzle_input: Vec<String>,
+    matching_boxes: Option<(String, String)>
 }
 
-impl Counts {
-    fn new() -> Counts {
-        Counts {
-            two: 0,
-            three: 0
+impl PartTwo {
+    fn new(file_name: &str) -> PartTwo {
+        let mut file = File::open(file_name).unwrap();
+        let mut puzzle_input = String::new();
+
+        file.read_to_string(&mut puzzle_input).unwrap();
+
+        let puzzle_input: Vec<String> = puzzle_input.trim().lines().map(|s| String::from(s)).collect();
+        let matching_boxes = None;
+
+        PartTwo {
+            puzzle_input,
+            matching_boxes
         }
     }
 
-    fn increment_two(&mut self) {
-        self.two += 1;
+    fn find_correct_boxes(&mut self) {
+        for (index, first_box) in self.puzzle_input.clone().iter().enumerate() {
+            self.matches(first_box.to_owned(), index);
+        }
     }
 
-    fn increment_three(&mut self) {
-        self.three += 1;
+    fn matches(&mut self, first_box: String, index: usize) {
+        let mut i = index + 1;
+
+        for _ in i..self.puzzle_input.len() {
+            let other_box = self.puzzle_input[i].to_owned();
+            
+            if self.is_matching_box(first_box.clone(), other_box.clone()) {
+                self.matching_boxes = Some((first_box.clone(), other_box.clone()));
+            }
+            i += 1;
+        }
     }
-}
 
-fn get_input_file(file_name: &str) -> String {
-    let mut file = File::open(file_name).unwrap();
-    let mut result = String::new();
+    fn is_matching_box(&self, first_box: String, second_box: String) -> bool {
+        let first: Vec<char> = first_box.chars().collect();
+        let second: Vec<char> = second_box.chars().collect();
+        let mut off_character_count = 0;
 
-    file.read_to_string(&mut result).unwrap();
-    result
+        for (index, first_letter) in first.iter().enumerate() {
+            let second_letter = second[index];
+
+            if *first_letter != second_letter {
+                off_character_count += 1;
+            }
+        }
+
+        off_character_count <= 1
+    }
+
+    fn get_correct_box_id(&self) -> Result<String, String> {
+        match self.matching_boxes {
+            Some((ref first, ref second)) => {
+                let mut result = String::new();
+                let first_letters: Vec<char> = first.chars().collect();
+                let second_letters: Vec<char> = second.chars().collect();
+
+                for (index, first_letter) in first_letters.iter().enumerate() {
+                    if *first_letter == second_letters[index] {
+                        result.push(*first_letter);
+                    }
+                }
+
+                Ok(result)
+            },
+            _ => Err(String::from("no match found"))
+        }
+    }
 }
 
 fn main() {
-    // get the file
-    let puzzle_input = get_input_file("puzzle_input");
-    let lines: Vec<&str> = puzzle_input.trim().lines().collect();
-    // count the repeating letters
-    let mut counts = Counts::new();
+    let mut part_two = PartTwo::new("puzzle_input");
 
-    for line in lines {
-        let mut letters = HashMap::new();
-        
-        for c in line.chars() {
-            let counter = letters.entry(c).or_insert(0);
-            *counter += 1;
-        }
-        // increment the two counts
-        // increment the three counts
-        let mut got_two = false;
-        let mut got_three = false;
+    part_two.find_correct_boxes();
 
-        for (_letter, count) in letters.iter() {
-            if !got_two && *count == 2 {
-                counts.increment_two();
-                got_two = true;
-            } else if !got_three && *count == 3 {
-                counts.increment_three();
-                got_three = true;
-            }
-        }
-    }
-    // multiply them together
-    println!("hash: {}", counts.two * counts.three);
+    let correct_box = part_two.get_correct_box_id().unwrap();
+
+    println!("{}", correct_box);
 }
